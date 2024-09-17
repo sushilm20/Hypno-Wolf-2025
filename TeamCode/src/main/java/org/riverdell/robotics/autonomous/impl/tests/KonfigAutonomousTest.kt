e916e24351272003f428912cb42c93cff8c152fc
Subham <growlyxdev@gmail.com>
2024-09-17 09:56:10 -0400

    feat: add definition

---
+++ TeamCode/src/main/java/org/riverdell/robotics/autonomous/impl/tests/KonfigAutonomousTest.kt
@@ -0,0 +1,15 @@
+package org.riverdell.robotics.autonomous.impl.tests
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.riverdell.robotics.autonomous.movement.konfig.KonfigAutonomous
+
+@Autonomous(name = "Test | Konfig", group = "Test")
+class KonfigAutonomousTest : KonfigAutonomous("testing")
+{
+    override fun definitions()
+    {
+        action("pickup_sample") {
+            // do work here
+        }
+    }
+}
